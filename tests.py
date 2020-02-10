94e9a9ce26409cbf5f6853790d88a9193e1a1eba
arnoldmw <thremulate@gmail.com>
2020-02-10 12:30:07 +0300

    Test for user_edit

--- tests.py
+++ tests.py
@@ -103,7 +103,14 @@ class UserManagement(AioHTTPTestCase):
         self.assertTrue(resp_two.status == 200, msg="Failed to access /user_profile. Received status code {0}"
                         .format(resp_two.status))
 
-
+    @unittest_run_loop
+    async def test_user_index(self):
+        resp = await self.client.request("POST", "/login_post", data=data)
+        self.assertTrue(resp.status == 200, msg="Failed to access /login. Received status code {0}"
+                        .format(resp.status))
+        resp_two = await self.client.request("GET", "/user_edit")
+        self.assertTrue(resp_two.status == 200, msg="Failed to access /user_edit. Received status code {0}"
+                        .format(resp_two.status))
 class AdversaryTests(AioHTTPTestCase):
     async def get_application(self):
         app = await create_app()
