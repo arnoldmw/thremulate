7b2784318b8456cee00b710e009130434ed771ab
arnoldmw <thremulate@gmail.com>
2020-03-23 23:28:55 +0300

    Refactored index.html

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ async def home(request):
     await check_authorized(request)
     session = await get_session(request)
     current_user = session['current_user']
-    return {'current_user': current_user, 'title': ""}
+    return {'current_user': current_user, 'title': 'Home'}
 
 
 def setup_logging():
