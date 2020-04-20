81c80f4f3684d401279b8c9bd23a5af62eb8b230
arnoldmw <thremulate@gmail.com>
2020-04-20 23:28:47 +0300

    Added license to files

--- modules/user_mgt.py
+++ modules/user_mgt.py
@@ -1,3 +1,19 @@
+#      Thremulate executes Network Adversary Post Compromise Activities.
+#      Copyright (C) 2020  Mwesigwa Arnold
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import aiohttp_jinja2
 from aiohttp import web
 from aiohttp_security import (
